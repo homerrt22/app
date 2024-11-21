ac0b4bcccc4aae451b3bff07eceb67c024699e71
KLOUD <oleg-penkov.2007@mail.ru>
2024-11-21 11:14:58 +0500

    MyTherdCommit

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+BOX = []
 def main():
     for i in range(1,100):
-        print(i)
+        BOX.append(i)
+        return BOX
 
-main()
+
+def boubleSour(BOX = []):
+    tmp = 0
+    for j in BOX:
+        for t in BOX + 1:
+            tmp = BOX[t]
+            if BOX[j] > BOX[t + 1]:
+                BOX[t + 1], BOX[t]
+    print(BOX)
+    
+main(boubleSour(BOX))
